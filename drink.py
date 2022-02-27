002c3119faf6bdcb80c5902daf91bcc957008b8d
Davide Furlani <davide.furlani0998@gmail.com>
2022-02-27 09:18:25 +0100

    fine

---
+++ drink.py
@@ -0,0 +1,8 @@
+from notifypy import Notify
+
+notification = Notify()
+notification.application_name = "Ci penso io a te"
+notification.title = "BEVI!!"
+notification.message = "Beviiiiiiiiiiiiiiiiii"
+notification.icon = "brindis-with-wine-glasses.png"
+notification.send()
---
+++ look.py
@@ -0,0 +1,8 @@
+from notifypy import Notify
+
+notification = Notify()
+notification.application_name = "Ci penso io a te"
+notification.title = "EHI!!"
+notification.message = "Distogli lo suardo"
+notification.icon = "eyeglasses.png"
+notification.send()
