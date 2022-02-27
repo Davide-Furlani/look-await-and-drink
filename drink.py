fb3d236e8eb3b88b24e1889a42bd7b2782c4a75b
Davide <davide.furlani0998@gmail.com>
2022-02-27 13:17:07 +0100

    aggiunto suono alla notifica

--- drink.py
+++ drink.py
@@ -5,4 +5,5 @@ notification.application_name = "Ci penso io a te"
 notification.title = "BEVI!!"
 notification.message = "Beviiiiiiiiiiiiiiiiii"
 notification.icon = "brindis-with-wine-glasses.png"
+notification.audio = "drip.wav"
 notification.send()
--- look.py
+++ look.py
@@ -5,4 +5,5 @@ notification.application_name = "Ci penso io a te"
 notification.title = "EHI!!"
 notification.message = "Distogli lo suardo"
 notification.icon = "eyeglasses.png"
+notification.audio = "drip.wav"
 notification.send()
